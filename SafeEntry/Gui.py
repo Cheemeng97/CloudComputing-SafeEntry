3db5f76d64d8ed9c12717498ef67eb11ddb17db3
Oon Chee Meng <34565690+Cheemeng97@users.noreply.github.com>
2022-06-09 11:07:50 +0800

    Simple GUI

--- SafeEntry/Gui.py
+++ SafeEntry/Gui.py
@@ -1,16 +1,46 @@
 import PySimpleGUI as sg
 
-layout = [[sg.Text("SafeEntry Check In")], [sg.Button("OK")]]
+#Check in out tab
+checkin_layout = [[
+    sg.Text("SafeEntry Check In")], 
+    [sg.Radio('Koufu', 'place', default=True) ,
+           sg.Radio('Foodgle', 'place')
+           ,sg.Radio('South Canteen', 'place')
+           ,sg.Radio('North Canteen', 'place')],
+    [sg.Button("Checkin")], 
+    [sg.Button("Checkout")]
+    ]
+
+#History tab
+history_layout = [[sg.Text('History', background_color='tan1')],
+               [sg.Input(key='-in2-')]]
+
+#Notification tab
+notification_layout = [[sg.Text('Notification', background_color='tan1')],
+               [sg.Input(key='-in2-')]]
+
+
+# tab group
+tab_layout = [[sg.TabGroup([[
+    sg.Tab('Check In', checkin_layout), 
+    sg.Tab('History', history_layout),
+    sg.Tab('Notification', notification_layout)]])]]
+
+
+
 
 # Create the window
-window = sg.Window("Safe Entry", layout)
+window = sg.Window("Safe Entry", tab_layout)
 
 # Create an event loop
 while True:
     event, values = window.read()
+    print(event, values)
     # End program if user closes window or
     # presses the OK button
-    if event == "OK" or event == sg.WIN_CLOSED:
+    if event == "Checkin":
+        break
+    if event == "Checkout":
         break
 
 window.close()
